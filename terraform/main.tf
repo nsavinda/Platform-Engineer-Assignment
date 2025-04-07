d9475274df52431c3e3d0070d4558b7fd20f769d
nsavinda <nirmalsavinda29@gmail.com>
2025-04-07 23:30:20 +0530

    Add IAM policy for ECS tasks to access GitLab registry secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -66,11 +66,35 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
+resource "aws_iam_policy" "ecs_secrets_access" {
+  name        = "ecs-task-secrets-access"
+  description = "Allows ECS tasks to access container registry secret"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "secretsmanager:GetSecretValue"
+        ],
+        Resource = aws_secretsmanager_secret.gitlab_registry_secret.arn
+      }
+    ]
+  })
+}
+
+
 # Attach Policies to Role
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_secrets_access_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_secrets_access.arn
+}
+
 
 # ECS Task Definition
 resource "aws_ecs_task_definition" "app" {
